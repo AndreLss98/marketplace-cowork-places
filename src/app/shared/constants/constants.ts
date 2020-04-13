65f321653af1271c2a2786d5954d5132a703b2fe
Pedro <pedroramosdiehl@gmail.com>
2020-04-13 20:07:03 -0300

    atualizado rotas

--- src/app/shared/constants/constants.ts
+++ src/app/shared/constants/constants.ts
@@ -1,3 +1,7 @@
-export const HEADER_MENU_OPTIONS = ['Anuncie', 'Alugue', 'Ajuda']
+export const HEADER_MENU_OPTIONS = [
+    {name: 'Anuncie', rout: '/home'},
+    {name: 'Alugue', rout: '/search'},
+    {name: 'Ajuda', rout: '/home'}, 
+    ]
 
 export const USER_SESSION = 'user_data';
