e91d39ec46ff9a0abebee3fdf08b6d19b2a853b5
Pedro <pedroramosdiehl@gmail.com>
2020-04-20 17:54:34 -0300

    tirado efeito do scroll do search box, passou para search

--- src/app/modulos/search/components/searchbox/searchbox.component.ts
+++ src/app/modulos/search/components/searchbox/searchbox.component.ts
@@ -44,7 +44,6 @@ export class SearchboxComponent implements OnInit {
     
   ngOnInit(): void {
 
-    this.jquery();
     this.location.valueChanges.subscribe(data=>{
       console.log(data)
     })
@@ -78,29 +77,6 @@ export class SearchboxComponent implements OnInit {
         maxArea: maxArea.value
       }, skipLocationChange: true},
       )
-  }
-
-  jquery(){
-    $(document).ready(function() {
-
-      $(window).scroll((event)=>{
-        checkOffset();
-      });
-
-      function checkOffset() {
-        if($('#searchBox').offset().top + $('#searchBox').height() >= $('#footer').offset().top - 275){
-          console.log("entrei aqui")
-          $('#searchBox').css('position', 'absolute');
-          $('#searchBox').css('top', ($('#footer').offset().top - $('#searchBox').height() - 275) +'px');
-        }
-        if($(document).scrollTop() + window.innerHeight < $('#footer').offset().top){
-          
-          console.log('entrei')
-          $('#searchBox').css('position', 'fixed')
-          $('#searchBox').css('top', 'auto')
-        }
-    }
-    });
   }
 
 }
