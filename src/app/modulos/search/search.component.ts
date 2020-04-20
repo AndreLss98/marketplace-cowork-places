d5777a3e7b9a11e32c599aff98b144873f52f2cf
Pedro <pedroramosdiehl@gmail.com>
2020-04-20 19:47:32 -0300

    Atualizado eventos de scroll

--- src/app/modulos/search/search.component.ts
+++ src/app/modulos/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { HighlightService } from 'src/app/service/highlight.service';
 
@@ -11,15 +11,19 @@ export class SearchComponent implements OnInit {
   
   public result;
   private quantity:number = 20;
+  private offset = 'auto';
+  private position;
+
 
   constructor(
     private router: Router,
     private route: ActivatedRoute,
     private highlight: HighlightService
-  ) { }
+  ) {
+    this.position = $(window).scrollTop(); 
+   }
 
   ngOnInit(): void {
-    this.jquery();
     this.result = this.route.snapshot.data.source;
     this.route.queryParams.subscribe( (data) => {
       console.log("Tem algo aqui", data)
@@ -29,58 +33,61 @@ export class SearchComponent implements OnInit {
     })
   }
 
-  doSearch(){
-    this.router.navigateByUrl("/spaces")
+  ngOnDestroy(): void {
+    //Called once, before the instance is destroyed.
+    //Add 'implements OnDestroy' to the class.
+    $('#navTop').removeClass('hide');
+    $('#navTop').addClass('up');
   }
 
-  onScroll(){
-    if(this.quantity < 50){
+  @HostListener('window:scroll', ['$event']) // for window scroll events
+  scrollEvent(event) {
+    this.checkOffset();
+    var scroll = $(window).scrollTop();
+    // scroll to bottom
+    if(scroll > this.position + 30) {  
+        $('#navTop').removeClass('up');
+        $('#navTop').addClass('hide');
+
+      if($(document).scrollTop() + window.innerHeight > $('#footer').offset().top){
+        $("#searchScroll").removeClass("slideDown");
+        $("#searchScroll").addClass("slideUp");
+      }else{
+        $("#searchScroll").removeClass("slideUp");
+        $("#searchScroll").addClass("slideDown");
+      }
+        // this.offset = '20px'
+        this.position = scroll;
+    // Scroll to top
+    } else if (scroll < this.position){
+        $('#navTop').removeClass('hide');
+        $('#navTop').addClass('up');
+
+        $("#searchScroll").removeClass("slideDown");
+        $("#searchScroll").addClass("slideUp");
+        this.position = scroll;
+        // this.offset = 'auto';
+    }
+  }
+
+  onScrollContent(){
+    if(this.quantity < 20){
       this.quantity = this.quantity + 10;
     }
     this.result = this.highlight.getSomeSpaces(this.quantity)
   }
 
-  jquery(){
-    $(document).ready(function() {
+  private checkOffset() {
+    if($('#searchScroll').offset().top + $('#searchScroll').height() >= $('#footer').offset().top - 125){
 
-      $(window).scroll((event)=>{
-        checkOffset();
-      });
-
-      var offset = 'auto';
-
-      function checkOffset() {
-        if($('#searchScroll').offset().top + $('#searchScroll').height() >= $('#footer').offset().top){
-          console.log("entrei aqui")
-          $('#searchScroll').css('position', 'absolute');
-          $('#searchScroll').css('top', ($('#footer').offset().top - $('#searchScroll').height()) +'px');
-        }
-        if($(document).scrollTop() + window.innerHeight < $('#footer').offset().top){
-          
-          console.log('entrei')
-          $('#searchScroll').css('position', 'fixed')
-          $('#searchScroll').css('top', offset)
-        }
-      }
-      var position = $(window).scrollTop(); 
-
-      $(window).scroll(function() {
-        var scroll = $(window).scrollTop();
-        if(scroll > position + 30) {
-            $('#navTop').removeClass('up');
-            $('#navTop').addClass('hide');
-            offset = '20px'
-            position = scroll;
-        } else if (scroll < position){
-            $('#navTop').removeClass('hide');
-            $('#navTop').addClass('up');
-            position = scroll;
-            offset = 'auto';
-        }
-      });
-    });
-
+      $('#searchScroll').css('position', 'absolute');
+      $('#searchScroll').css('top', ($('#footer').offset().top - $('#searchScroll').height()) - 125 +'px');
+    }
+    if($(document).scrollTop() + window.innerHeight < $('#footer').offset().top){
+      
+      $('#searchScroll').css('position', 'fixed')
+      $('#searchScroll').css('top', this.offset)
+    }
   }
-
 
 }
