bd25ff68207ce89b89cf1f0ba528917c5bea4196
Pedro <pedroramosdiehl@gmail.com>
2020-05-19 13:37:56 -0300

    Header atualizado

--- src/app/shared/constants/constants.ts
+++ src/app/shared/constants/constants.ts
@@ -5,12 +5,12 @@ export const HEADER_NAV_OPTIONS = [
     ]
 
 export const HEADER_MENU_OPTIONS = [
-    {name:'Sala de Reunião' , img: 'assets/imgs/header/sala_reuniao1x.png', rout: '/home'},
-    {name:'Mesas' , img: 'assets/imgs/header/mesas1x.png', rout: '/home'},
-    {name:'Salas Privativas' , img: 'assets/imgs/header/sala_privativa1x.png', rout: '/home'},
-    {name:'Auditórios' , img: 'assets/imgs/header/auditorios1x.png', rout: '/home'},
-    {name:'Serviços' , img: 'assets/imgs/header/servicos1x.png', rout: '/home'},
-    {name:'Palestras e Cursos' , img: 'assets/imgs/header/palestras1x.png', rout: '/home'},
+    {name:'Sala de Reunião' , img: 'assets/imgs/header/sala_reuniao.png', rout: '/home'},
+    {name:'Mesas' , img: 'assets/imgs/header/mesas.png', rout: '/home'},
+    {name:'Salas Privativas' , img: 'assets/imgs/header/salas_privativas.png', rout: '/home'},
+    {name:'Auditórios' , img: 'assets/imgs/header/auditorios.png', rout: '/home'},
+    {name:'Serviços' , img: 'assets/imgs/header/servicos.png', rout: '/home'},
+    {name:'Palestras e Cursos' , img: 'assets/imgs/header/palestras.png', rout: '/home'},
 ]
 
 export const USER_SESSION = 'user_data';
