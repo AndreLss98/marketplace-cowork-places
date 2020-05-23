2dbfa3662a25fbadf24ea91eaac9bd85bfaf161f
pedro <pedroramosdiehl@gmail.com>
2020-05-23 14:42:04 -0300

    env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  loginUrl: 'http://placeet.com.br:9011/oauth2/authorize?client_id=445ebce3-618a-42b5-b746-95ea441766e6&response_type=code&redirect_uri=http%3A%2F%2Fplaceet.com.br',
+  apiUrl: 'http://206.189.168.26',
+  token: ''
 };
