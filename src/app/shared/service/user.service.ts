ea5c7bcf1209243f011bcc116ff663e6a1326fcf
pedro <pedroramosdiehl@gmail.com>
2020-06-02 16:18:39 -0300

    adicionado método para verificar sessão

--- src/app/shared/service/user.service.ts
+++ src/app/shared/service/user.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 
@@ -13,5 +13,12 @@ export class UserService {
 
   public getUserById(id: number){
     return this.http.get(environment.apiUrl + '/usuarios/' + id);
+  }
+
+  public verifyUserEmail(email: string){
+    let params = new HttpParams();
+    let headers = new HttpHeaders();
+    params = params.append('email', email);
+    return this.http.post(environment.apiUrl + '/usuarios/email', {email: email});
   }
 }
