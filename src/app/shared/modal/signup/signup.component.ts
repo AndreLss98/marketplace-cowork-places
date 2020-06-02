105c0279ea6707527423adf75986aebbd6f629c7
pedro <pedroramosdiehl@gmail.com>
2020-06-02 16:19:10 -0300

    Adicionado validação de alguns campos

--- src/app/shared/modal/signup/signup.component.ts
+++ src/app/shared/modal/signup/signup.component.ts
@@ -1,3 +1,4 @@
+import { UserService } from './../../service/user.service';
 import { LoginService } from 'src/app/shared/service/login.service';
 import { User } from './../../interface/interface';
 import { ModalService } from 'src/app/shared/service/modal.service';
@@ -10,6 +11,7 @@ import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE, ErrorStateMatcher } fro
 import { LoginComponent } from '../login/login.component';
 import { MatStepper } from '@angular/material/stepper';
 import { Passos, emailPattern } from '../../constants/constants';
+import * as moment from 'moment';
 
 export class MyErrorStateMatcher implements ErrorStateMatcher {
   isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
@@ -43,7 +45,7 @@ export class SignupComponent implements OnInit {
   matcher = new MyErrorStateMatcher();
 
   public primeiroPasso: FormGroup;
-  email = new FormControl('', [Validators.required, Validators.pattern(emailPattern)]);
+  email = new FormControl('', [Validators.required, Validators.email, Validators.pattern(emailPattern)]);
 
   public segundoPasso: FormGroup;
   nome = new FormControl('', [Validators.required, Validators.pattern('')]);
@@ -56,14 +58,15 @@ export class SignupComponent implements OnInit {
   tel2 = new FormControl('');
 
   public quartoPasso: FormGroup;
-  senha = new FormControl('', Validators.required)
-  confirmar_senha = new FormControl('', Validators.required);
+  senha = new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength(16)])
+  confirmar_senha = new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength(16)]);
 
   constructor(
     private signup: SignupService,
     private form: FormBuilder,
     private modal: ModalService,
-    private login: LoginService
+    private login: LoginService,
+    private user: UserService
   ){
     
     // Inicia o formulario.
@@ -92,30 +95,59 @@ export class SignupComponent implements OnInit {
   verificaSenhas(group: FormGroup){
     let pass = group.controls.senha.value;
     let confirmPass = group.controls.confirmar_senha.value;
+    if ( pass.length < 6) return {lengthMin: true}
+    if ( pass.length > 16) return {lengthMax: true}
     return pass === confirmPass ? null : { notSame: true }
   }
-
   ngOnInit(): void {
   }
 
   nextStep(stepper: MatStepper){
     switch (stepper.selectedIndex) {
       case Passos.Primeiro:
-        // Verificar se o email já existe 
-        nextStep();
+
+        let email = this.primeiroPasso.value.email;
+        console.log(this.primeiroPasso.valid)
+      
+        this.user.verifyUserEmail(email)
+          .subscribe( response => {
+            console.log(response)
+            this.primeiroPasso.setErrors({existe: true});
+            console.log(this.primeiroPasso.valid)
+          }, err => {
+            if(err.status == 404){
+              this.primeiroPasso.setErrors(null)
+              nextStep();
+            }
+          })
         break;
+      case Passos.Segundo:
+
+      let nascimento = this.segundoPasso.controls.data_nascimento.value;
+      let age = moment().diff(nascimento, 'years')
+      
+      if(age < 18){
+        this.segundoPasso.setErrors({age: true})
+      }else{
+        nextStep();
+      }
+
+      break;
       case Passos.Quarto:
         // Valida Senha
+        console.log(this.quartoPasso.errors, this.quartoPasso.valid)
         this.editavel = false;
         this.loader = true;
-        this.signup.cadastrar(this.criarUsuario()).subscribe( response => {
-          this.login.signInAfterSignup(response);
-          nextStep();
-        }, error => {
-          console.log("Cadastro invalido",error)
-          this.editavel = true;
-          this.loader = false;
-        })
+        if(this.quartoPasso.valid){
+          this.signup.cadastrar(this.criarUsuario()).subscribe( response => {
+            this.login.login(response);
+            nextStep();
+          }, error => {
+            console.log("Cadastro invalido",error)
+            this.editavel = true;
+            this.loader = false;
+          })
+        }
         break;
       default:
         nextStep();
@@ -135,7 +167,8 @@ export class SignupComponent implements OnInit {
       nome: this.segundoPasso.value.nome,
       sobrenome: this.segundoPasso.value.sobrenome,
       data_nascimento: this.segundoPasso.value.data_nascimento,
-      numero: this.terceiroPasso.value.tel1,
+      numero_1: this.terceiroPasso.value.tel1,
+      numero_2: this.terceiroPasso.value.tel2 ? 'null': this.terceiroPasso.value.tel2,
       senha: this.quartoPasso.value.senha,
       img_perfil: 'perfil.png'
     }
