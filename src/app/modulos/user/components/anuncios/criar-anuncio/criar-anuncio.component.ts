8d77ac39c1e0fff736dc8686e3f6fb9a6b46af6f
pedro <pedroramosdiehl@gmail.com>
2020-06-18 12:42:31 -0300

    adicionado form group dos dados cadastrais

--- src/app/modulos/user/components/anuncios/criar-anuncio/criar-anuncio.component.ts
+++ src/app/modulos/user/components/anuncios/criar-anuncio/criar-anuncio.component.ts
@@ -1,5 +1,7 @@
+import { IbgeService } from './../../../../../shared/service/ibge.service';
+import { environment } from './../../../../../../environments/environment';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-criar-anuncio',
@@ -8,24 +10,52 @@ import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
 })
 export class CriarAnuncioComponent implements OnInit {
 
+  // Estados e distritos
+  public estados;
+  public distritos;
+
+  // Politica de uso
+  public politica_de_uso = environment.apiUrl + '/md/politica_de_uso.md';
+
+  // Dados cadastrais
   public dados_cadastrais: FormGroup;
-  titulo = new FormControl();
-  tipo = new FormControl();
-  local = {
-    cep: new FormControl(),
-    pais: new FormControl(),
-    rua: new FormControl(),
-    cidade: new FormControl(),
-    estado: new FormControl(),
-    complemento: new FormControl()
-  }
+  titulo = new FormControl('', [Validators.required]);
+  tipo = new FormControl('', [Validators.required]);
+  descricao = new FormControl('', [Validators.required]);
+  cep = new FormControl('', [Validators.required]);
+  pais = new FormControl('', [Validators.required]);
+  rua = new FormControl('', [Validators.required]);
+  bairro = new FormControl('', [Validators.required]);
+  numero = new FormControl('');
+  cidade = new FormControl('', [Validators.required]);
+  estado = new FormControl('', [Validators.required]);
+  complemento = new FormControl('', [Validators.required]);
 
 
   constructor(
     private form: FormBuilder,
-  ) { }
+    private ibge: IbgeService
+  ) {
+    this.dados_cadastrais = this.form.group({
+      titulo: this.titulo,
+      tipo: this.tipo,
+      descricao: this.descricao,
+      cep: this.cep,
+      pais: this.pais,
+      rua: this.rua,
+      bairro: this.bairro,
+      numero: this.numero,
+      cidade: this.cidade,
+      estado: this.estado,
+      complemento: this.complemento
+    })
+  }
 
   ngOnInit(): void {
+    this.ibge.getEstados().subscribe( response => {
+      console.log(response);
+      this.estados = response;
+    })
   }
 
 }
