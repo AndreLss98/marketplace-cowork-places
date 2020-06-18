e902dd787c509524a8f2e07d3425381485254de2
pedro <pedroramosdiehl@gmail.com>
2020-06-18 12:42:06 -0300

    Adicionado mdviewer ao shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,17 +1,20 @@
+import { MdViewerComponent } from './components/md-viewer/md-viewer.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { MaterialModule } from './materia.module';
 import { NgModule } from '@angular/core';
+import { MarkdownModule } from 'ngx-markdown';
 import { HighlightItemsComponent } from './components/highlight-items/highlight-items.component';
 import { CommonModule } from '@angular/common';
 
 @NgModule({
-  declarations: [HighlightItemsComponent],
+  declarations: [HighlightItemsComponent, MdViewerComponent],
   imports: [
     CommonModule,
     MaterialModule,
     ReactiveFormsModule,
+    MarkdownModule,
     FormsModule
   ],
-  exports: [FormsModule, MaterialModule, ReactiveFormsModule, HighlightItemsComponent]
+  exports: [FormsModule, MaterialModule, ReactiveFormsModule, HighlightItemsComponent, MdViewerComponent]
 })
 export class SharedModule { }
