6e49d2b6197bdea68e55902f77de778daafaeaf5
pedro <pedroramosdiehl@gmail.com>
2020-06-18 12:41:11 -0300

    Adicionado md viewer, adicionado service do ibge

---
+++ src/app/shared/service/ibge.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class IbgeService {
+
+  private baseUrl = 'https://servicodados.ibge.gov.br/api/v1/localidades/'
+
+  constructor(
+    private http:HttpClient
+  ) { }
+
+  getEstados(){
+    return this.http.get(this.baseUrl + 'estados?orderBy=nome');
+  }
+
+  getCidadesPorEstado(uf:number){
+    return this.http.get(this.baseUrl + 'estados/' + uf + '/distritos');
+  }
+}
