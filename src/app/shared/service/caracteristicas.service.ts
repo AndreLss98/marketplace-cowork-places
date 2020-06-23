39eadeae66b8b98aac2f013faa441b0f19d6716d
pedro <pedroramosdiehl@gmail.com>
2020-06-22 23:53:16 -0300

    Adiconado observable

--- src/app/shared/service/caracteristicas.service.ts
+++ src/app/shared/service/caracteristicas.service.ts
@@ -1,3 +1,4 @@
+import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
@@ -13,8 +14,8 @@ export class CaracteristicasService {
 
   }
 
-  public getAll() {
-    return this.http.get(`${environment.apiUrl}/caracteristicas`);
+  public getAll(): Observable<any> {
+    return this.http.get<any>(`${environment.apiUrl}/caracteristicas`);
   }
 
   public save(politica) {
