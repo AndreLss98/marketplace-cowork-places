d50a0cf21a17bb6cd81eca973b1b5d84329fffd7
pedro <pedroramosdiehl@gmail.com>
2020-07-03 00:13:00 -0300

    adicionado resolver

---
+++ src/app/modulos/spaces/resolver/spaces-resolver.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { AlugaveisService } from 'src/app/shared/service/alugaveis.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SpacesResolverService implements Resolve<any>{
+
+  constructor(
+    private alugaveis: AlugaveisService
+  ) { }
+
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    return this.alugaveis.getById(route.paramMap.get('id'));
+  }
+}
--- src/app/modulos/spaces/spaces-routing.module.ts
+++ src/app/modulos/spaces/spaces-routing.module.ts
@@ -1,9 +1,18 @@
+import { SpacesResolverService } from './resolver/spaces-resolver.service';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { SpacesComponent } from './spaces.component';
 
-const routes: Routes = [{ path: '', component: SpacesComponent }];
+const routes: Routes = [
+  { 
+    path: '', 
+    component: SpacesComponent,
+    resolve: {
+      data: SpacesResolverService
+    }
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
