06915eaac3a7355ba2f4a4e0439e229e4b91ab0f
pedro <pedroramosdiehl@gmail.com>
2020-07-03 16:46:42 -0300

    Anuncio redirecionando pro checkout

--- src/app/modulos/spaces/spaces.component.ts
+++ src/app/modulos/spaces/spaces.component.ts
@@ -1,12 +1,15 @@
+import { AlugavelService } from 'src/app/shared/service/alugavel.service';
+import { CheckoutService } from './../../shared/service/checkout.service';
 import { element } from 'protractor';
 import { environment } from './../../../environments/environment';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { HighlightService } from 'src/app/shared/service/highlight.service';
 import { MatDatepickerInputEvent } from '@angular/material/datepicker';
 import * as moment from 'moment';
 import { MAT_DATE_LOCALE, DateAdapter, MAT_DATE_FORMATS } from '@angular/material/core';
 import { MomentDateAdapter, MAT_MOMENT_DATE_ADAPTER_OPTIONS, MAT_MOMENT_DATE_FORMATS } from '@angular/material-moment-adapter';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-spaces',
@@ -29,6 +32,9 @@ export class SpacesComponent implements OnInit {
   public saida;
   public hoje = moment().format();
 
+  // Valor maximo de taxa para calcular;
+  private max_taxa;
+
   public backUrl = environment.apiUrl;
   public espaco;
   public data = {
@@ -45,11 +51,18 @@ export class SpacesComponent implements OnInit {
   
   constructor(
     private highlight: HighlightService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private checkoutService: CheckoutService,
+    private alugavelService: AlugavelService,
+    private router: Router,
+    private snackBar: MatSnackBar
     ) { }
     
     ngOnInit(): void {
       console.log(this.hoje);
+      this.alugavelService.getTaxa().subscribe(response =>{
+        this.max_taxa = Number(response.taxa);
+      })
       this.espaco = this.route.snapshot.data['data'];
   
       console.log('Resolver: ', this.espaco);
@@ -64,6 +77,7 @@ export class SpacesComponent implements OnInit {
       //   this.espacos_similares = this.highlight.getSomeSpaces(4);
       // }
     }
+
     /**
      * Retorna um array com os nomes dos icones, para as estrelas da avalição
      * pode ser star, star_half, star_outline
@@ -89,13 +103,25 @@ export class SpacesComponent implements OnInit {
     return array;
   }
 
+  public checkout(){
+    if(this.entrada != undefined && this.saida != undefined){
+      this.checkoutService.data_entrada = this.entrada;
+      this.checkoutService.data_saida = this.saida;
+      this.checkoutService.espaco = this.espaco;
+  
+      this.router.navigate(['/checkout']);
+    }else{
+      this.snackBar.open('Selecione as datas de entrada e saída', 'OK', {duration: 3000})
+    }
+  }
+
   public calculaCustoDia(taxa, custo_dia): number{
-    if(taxa == 7){
+    if(taxa == this.max_taxa){
       return Number(custo_dia);
-    }else if(taxa == 3.5){
+    }else if(taxa == this.max_taxa/2){
       return Number(custo_dia * (taxa/100 + 1))
     }else{
-      return Number(custo_dia * 1.07)
+      return Number(custo_dia * (this.max_taxa/100 + 1))
     }
   }
 
@@ -113,7 +139,17 @@ export class SpacesComponent implements OnInit {
     if(a == undefined || b == undefined){ 
       return Number(this.calculaTotal(taxa, custo_dia));
     }else{
-      return Number(a.diff(b, 'days') * this.calculaTotal(taxa, custo_dia));
+      return Number((a.diff(b, 'days') + 1) * this.calculaTotal(taxa, custo_dia));
+    }
+  }
+
+  public totalDias(){
+    let b = this.entrada;
+    let a = this.saida;
+    if(a == undefined || b == undefined){ 
+      return Number(1);
+    }else{
+      return Number(a.diff(b, 'days') + 1);
     }
   }
 
