700f243cf19b0ccf97398defee34215dfbd1b737
pedro <pedroramosdiehl@gmail.com>
2020-07-10 23:23:33 -0300

    guard para edição de anuncio

---
+++ src/app/modulos/user/guard/editar-anuncio.guard.ts
@@ -0,0 +1,33 @@
+import { AlugaveisService } from 'src/app/shared/service/alugaveis.service';
+import { UserService } from 'src/app/shared/service/user.service';
+import { AlugavelService } from 'src/app/shared/service/alugavel.service';
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EditarAnuncioGuard implements CanActivate {
+
+  constructor(
+    private alugaveis: AlugaveisService,
+    private user: UserService
+  ){}
+
+  
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
+    return this.alugaveis.getById(next.queryParams.id).pipe(
+        map(res => {
+          console.log(res.anunciante_id, this.user.user_data.id, res.anunciante_id == this.user.user_data.id);
+          return res.anunciante_id == this.user.user_data.id;
+        }));
+
+    // return true;
+  }
+  
+}
