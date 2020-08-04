735ae7a3f89fc6e8a78db4e22bf467d26b20115e
pedro <pedroramosdiehl@gmail.com>
2020-08-04 10:26:27 -0300

    Removido scroll do filtro de busca

--- src/app/modulos/search/search.component.ts
+++ src/app/modulos/search/search.component.ts
@@ -1,5 +1,5 @@
 import { ActivatedRoute, Router } from '@angular/router';
-import { Component, OnInit, HostListener } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { MenuService } from 'src/app/shared/service/menu.service';
 import { HighlightService } from 'src/app/shared/service/highlight.service';
@@ -45,52 +45,7 @@ export class SearchComponent implements OnInit {
     $('#navTop').addClass('up');
   }
 
-  // @HostListener('window:scroll', ['$event']) // for window scroll events
-  // scrollEvent(event) {
-
-  //   this.checkOffset();
-
-  //   var scroll = $(window).scrollTop();
-
-  //   // scroll to bottom
-  //   if (scroll > this.position + 50) {
-  //     $('#navTop').removeClass('up');
-  //     $('#navTop').addClass('hide');
-
-  //     if ($(document).scrollTop() + window.innerHeight > $('#footer').offset().top) {
-  //       $("#searchScroll").removeClass("slideDown");
-  //       $("#searchScroll").addClass("slideUp");
-  //     } else {
-  //       $("#searchScroll").removeClass("slideUp");
-  //       $("#searchScroll").addClass("slideDown");
-  //     }
-  //     // this.offset = '20px'
-  //     this.position = scroll;
-  //     // Scroll to top
-  //   } else if (scroll < this.position) {
-  //     $('#navTop').removeClass('hide');
-  //     $('#navTop').addClass('up');
-
-  //     $("#searchScroll").removeClass("slideDown");
-  //     $("#searchScroll").addClass("slideUp");
-  //     this.position = scroll;
-  //     // this.offset = 'auto';
-  //   }
-  // }
-
   onScrollContent() {
     // this.result = this.highlight.getSomeSpaces(this.quantity)
   }
-
-  private checkOffset() {
-    if ($('#searchScroll').offset().top + $('#searchScroll').height() >= $('#footer').offset().top - 233) {
-      $('#searchScroll').css('position', 'absolute');
-      $('#searchScroll').css('top', ($('#footer').offset().top - $('#searchScroll').height()) - 233 + 'px');
-    }
-    if ($(document).scrollTop() + window.innerHeight < $('#footer').offset().top) {
-      $('#searchScroll').css('position', 'fixed')
-      $('#searchScroll').css('top', this.offset)
-    }
-  }
-
 }
