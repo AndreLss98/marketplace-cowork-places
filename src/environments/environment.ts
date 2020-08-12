445756e75f4a810299ab68db47629a7d3e95f6de
AndreLss <andre_luis98@outlook.com>
2020-08-12 16:01:01 -0300

    Configura ambiente de dev

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://homolog.backend.localhost',
+  apiUrl: 'http://backend.localhost',
   token: '',
   google_api_key: 'AIzaSyBBE4kMR4humYJfPb4MCxqqPF5geXZYAvk'
 };
