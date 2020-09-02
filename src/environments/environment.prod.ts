f66e4ecaa556246d730c7ac5dd095fe66b52b720
AndreLss <andre_luis98@outlook.com>
2020-09-02 14:47:40 -0300

    Adiciona token da paypal nas variaveis de ambiente

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: true,
   apiUrl: 'https://backend.placeet.com',
   token: '',
-  google_api_key: 'AIzaSyBBE4kMR4humYJfPb4MCxqqPF5geXZYAvk'
+  google_api_key: 'AIzaSyBBE4kMR4humYJfPb4MCxqqPF5geXZYAvk',
+  paypal_client_id: 'AQfkR-tJOEHy0SLaGH_x5cAk-0lIWVWPsKgzhHIgb3Rq5RD4bgSivIw32JNFR0za2vtWtnDF_YjbjP-w'
 };
