6b5623d7c6106773ca3a3ef839c4a7c8a765b4b0
AndreLss <andre_luis98@outlook.com>
2020-09-09 19:10:36 -0300

    Prototipacao de filtros reponsivos na tabela paginada

--- src/app/shared/components/filter-pageable-table/filter-pageable-table.component.ts
+++ src/app/shared/components/filter-pageable-table/filter-pageable-table.component.ts
@@ -4,11 +4,13 @@ import { FormGroup, FormControl } from '@angular/forms';
 import { PageableTableComponent } from '../pageable-table/pageable-table.component';
 
 export interface FormField {
+  type: 'text' | 'select';
   nome_campo: string;
   valor_inicial?: any;
   label: string;
   placeholder?: string;
   hint?: string;
+  options?: { value: string }[]
 }
 
 @Component({
@@ -24,14 +26,21 @@ export class FilterPageableTableComponent extends PageableTableComponent {
 
   constructor() {
     super();
+    this.formFields = [
+      { type: "text", label: "Teste", nome_campo: "nome", hint: "Ajuda é o carai" },
+      { type: "select", label: "Status", nome_campo: "status", options: [ { value: "Approved" }, { value: "Disapproved" } ] },
+      { type: "select", label: "Status", nome_campo: "status", options: [ { value: "Approved" }, { value: "Disapproved" } ] },
+      { type: "select", label: "Status", nome_campo: "status", options: [ { value: "Approved" }, { value: "Disapproved" } ] },
+      { type: "text", label: "TesteII", nome_campo: "sobrenome", hint: "Ajuda é o carai" }
+    ]
     // this.filters[]
   }
 
   ngOnInit(): void {
     this.filters = null;
-    this.formFields.forEach(field => {
-      this.filters[field.nome_campo] = new FormControl(field.valor_inicial || '');
-    });
+    // this.formFields.forEach(field => {
+    //   this.filters[field.nome_campo] = new FormControl(field.valor_inicial || '');
+    // });
   }
 
   public emitPageEvent(event) {
