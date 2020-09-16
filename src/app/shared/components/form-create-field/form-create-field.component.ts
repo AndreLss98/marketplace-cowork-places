7286ed20ac6792e7ebfbd0f7a8051ca1fcd65712
AndreLss <andre_luis98@outlook.com>
2020-09-16 13:19:22 -0300

    Ajsuta edicao de caracteristica

--- src/app/shared/components/form-create-field/form-create-field.component.ts
+++ src/app/shared/components/form-create-field/form-create-field.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { FormGroup, Validators, FormControl } from '@angular/forms';
 
 import { TIPOS_CAMPOS } from 'src/app/shared/constants/constants';
 
@@ -18,17 +18,26 @@ export class FormCreateFieldComponent implements OnInit {
 
   @Output()
   public possibilidadesChange = new EventEmitter();
+
+  @Input()
+  public editMode: boolean = false;
   public fieldForm: FormGroup;
   public propriedadesFields = [];
   private _possibilidades = [];
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  ngAfterViewInit() {
     this.fieldForm.controls['tipo_campo'].valueChanges.subscribe(() => {
+      this.editMode = false;
       if (this.fieldForm.controls['tipo_campo'].value) this.configForm();
     });
-  };
+    setTimeout(() => {
+      if (this.fieldForm.controls['tipo_campo'].value) this.configForm();
+    }, 100);
+  }
 
   @Input('original-form')
   get original_form() {
@@ -66,14 +75,17 @@ export class FormCreateFieldComponent implements OnInit {
     Object.keys(TIPOS_CAMPOS[this.fieldForm.controls['tipo_campo'].value].campos).forEach(field => {
       let tempField = TIPOS_CAMPOS[this.fieldForm.controls['tipo_campo'].value].campos[field];
       tempField.name = field;
-      group[field] = tempField.required ? new FormControl(tempField.type === 'boolean'? false : null, [Validators.required]) : new FormControl(tempField.type === 'boolean'? false : null);
+      if (!this.editMode) {
+        group[field] = tempField.required ? new FormControl(tempField.type === 'boolean'? false : null, [Validators.required]) : new FormControl(tempField.type === 'boolean'? false : null);
+      }
       fields.push(tempField);
     });
 
-    this.propriedadesForm = new FormGroup(group);
+    if (!this.editMode) this.propriedadesForm = new FormGroup(group);
     this.propriedadesForm.valueChanges.subscribe(() => {
       this.original_form.updateValueAndValidity();
-    })
+    });
+
     this.propriedadesFields = fields;
   }
 
