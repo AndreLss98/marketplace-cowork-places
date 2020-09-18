4490cd14e6cc8a198ffe3828a24131fde9fcd996
AndreLss <andre_luis98@outlook.com>
2020-09-18 08:59:20 -0300

    Formata valor da diaria visivel no componente de card de item de anuncio

--- src/app/shared/components/card-item-alugavel/card-item-alugavel.component.ts
+++ src/app/shared/components/card-item-alugavel/card-item-alugavel.component.ts
@@ -4,6 +4,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { environment } from 'src/environments/environment';
 
 import { formatMoneyValue } from '../../constants/functions';
+import { Financeiro } from 'src/app/shared/classes/financeiro';
 import { ENUM_ALUGAVEL_CARACTERISTICAS } from '../../constants/constants';
 
 import { AlugavelService } from 'src/app/shared/service/alugavel.service';
@@ -14,38 +15,41 @@ import { AlugaveisService } from 'src/app/shared/service/alugaveis.service';
   templateUrl: './card-item-alugavel.component.html',
   styleUrls: ['./card-item-alugavel.component.scss']
 })
-export class CardItemAlugavelComponent implements OnInit {
+export class CardItemAlugavelComponent extends Financeiro implements OnInit {
 
-  @Input('data') data: any = undefined;
-  @Input('width') width:string = '277px';
+  readonly formatMoneyValue = formatMoneyValue;
 
-  @Input('custom-redirect') custom_redirect: string = '';
+  @Input('data')
+  public data;
 
-  @Input('alugavel_id') alugavel_id: number = undefined;
+  @Input('custom-redirect')
+  public custom_redirect: string;
+
+  @Input('alugavel_id')
+  public alugavel_id: number;
 
   public backUrl = environment.apiUrl;
-  private taxaTotal;
+  public taxaTotal;
   public CARACTERISTICAS = ENUM_ALUGAVEL_CARACTERISTICAS;
 
   constructor(
     private router: Router,
     private alugavel: AlugavelService,
     private alugaveis: AlugaveisService
-  ) { }
+  ) {
+    super();
+  }
 
   ngOnInit(): void {
+    this.alugavel.getTaxa().subscribe(response => {
+      this.taxaTotal = response.taxa;
+    });
+    
     if(this.alugavel_id){
       this.alugaveis.getById(this.alugavel_id).subscribe( res => {
         this.data = res;
       });
     }
-
-    setTimeout(() => {      
-      this.data.valor = Number(this.data['valor'])
-      this.alugavel.getTaxa().subscribe( res => {
-        this.taxaTotal = res.taxa;
-      })
-    }, 10);
   }
 
   goToSpace() {
@@ -71,10 +75,5 @@ export class CardItemAlugavelComponent implements OnInit {
     }
     
     return array;
-  }
-
-  custoDia() {
-    return formatMoneyValue(Number (this.data.valor * ((this.taxaTotal / 100) + 1)).toFixed(2));
-    // return Number(this.data.valor * ( (this.taxaTotal / 100) + 1));
   }
 }
