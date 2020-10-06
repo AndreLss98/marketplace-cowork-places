d2f0e540d516016a6156d5800fa20a57b2ba5b92
AndreLss <andre_luis98@outlook.com>
2020-10-06 12:27:55 -0300

    Remove menu da tela de home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,10 @@
+import { Router } from '@angular/router';
 import { Component, ElementRef, ViewChild } from '@angular/core';
 
 import { UserService } from './shared/service/user.service';
 import { LoginService } from './shared/service/login.service';
+import { MatDialog } from '@angular/material/dialog';
+import { LoginComponent } from './shared/modal/login/login.component';
 
 @Component({
   selector: 'app-root',
@@ -17,8 +20,10 @@ export class AppComponent {
   public sidenavOpened = false;
 
   constructor(
-    private login: LoginService,
-    public userService: UserService
+    private router: Router,
+    private dialog: MatDialog,
+    public login: LoginService,
+    public userService: UserService,
   ) {
     this.login.autoLogin();
   }
@@ -26,6 +31,34 @@ export class AppComponent {
   toggleSideMenu() {
     this.sidenavOpened = !this.sidenavOpened;
     !this.sidenavOpened? this.nav.nativeElement.classList.add('sticky-top') : this.nav.nativeElement.classList.remove('sticky-top');
+  }
+
+  goTo(path: string, id?: number) {
+    let queryParams: any = {};
+    if (id) queryParams.tipo_id = id;
+    this.router.navigate([path], { queryParams });
+  }
+
+  openModalOrOpenPopover(popover?) {
+    if (!this.login.logged_status) {
+      this.dialog.open(LoginComponent);
+    } else {
+      popover.isOpen() ? popover.close() : popover.open();
+    }
+  }
+
+  comoFunciona() {
+    if (!this.router.url.includes('/home')) {
+      this.router.navigate(['/home']).then(() => {
+        setTimeout(() => {
+          var el = document.getElementById('comoFunciona');
+          el.scrollIntoView({ behavior: 'smooth' });
+        }, 100);
+      })
+    } else {
+      var el = document.getElementById('comoFunciona');
+      el.scrollIntoView({ behavior: 'smooth' });
+    }
   }
 }
 
