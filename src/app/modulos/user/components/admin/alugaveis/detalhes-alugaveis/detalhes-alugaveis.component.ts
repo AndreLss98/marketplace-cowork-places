68e9aec1009cb323dc7c6405d650202eda4b2cc0
Victor Monteiro Torres <victoorres@icloud.com>
2021-01-14 18:26:19 -0300

    Perfil de usuário em na hora de APROVAR o  anuncio

--- src/app/modulos/user/components/admin/alugaveis/detalhes-alugaveis/detalhes-alugaveis.component.ts
+++ src/app/modulos/user/components/admin/alugaveis/detalhes-alugaveis/detalhes-alugaveis.component.ts
@@ -1,4 +1,4 @@
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
@@ -8,6 +8,7 @@ import { AlugavelService } from 'src/app/shared/service/alugavel.service';
 
 import { formatMoneyValue, translateBoolValue } from 'src/app/shared/constants/functions';
 import { ALUGAVEL_STATUS, USUARIO_STATUS, TIPOS_CAMPOS } from 'src/app/shared/constants/constants';
+import { UserService } from 'src/app/shared/service/user.service';
 
 @Component({
   selector: 'app-detalhes-alugaveis',
@@ -20,21 +21,24 @@ export class DetalhesAlugaveisComponent implements OnInit {
 
   public alugavel: any;
   public backURL = `${environment.apiUrl}/`;
+  public imgUrl;
 
-  public displayedColumns = [ 'caracteristica', 'valor' ];
-  public displayedDocumentsColumns = [ 'documento', 'action' ];
+  public displayedColumns = ['caracteristica', 'valor'];
+  public displayedDocumentsColumns = ['documento', 'action'];
 
   public status = Object.values(ALUGAVEL_STATUS).filter(status => status.value !== ALUGAVEL_STATUS.REMOVED.value);
   readonly STATUS = ALUGAVEL_STATUS;
   readonly USUARIO_STATUS = USUARIO_STATUS;
 
   public statusForm: FormGroup;
-  
+
   constructor(
     private route: ActivatedRoute,
+    private router: Router,
     private formBuilder: FormBuilder,
     private alugavelService: AlugavelService,
-    private snack: MatSnackBar
+    private snack: MatSnackBar,
+    public userService: UserService
   ) {
     this.statusForm = formBuilder.group({
       status: ['', [Validators.required]],
@@ -44,14 +48,15 @@ export class DetalhesAlugaveisComponent implements OnInit {
 
   ngOnInit(): void {
     this.alugavel = this.route.snapshot.data['alugavel'];
-    
+
     this.alugavel.valor = Number(this.alugavel.valor);
     this.alugavel.valor_mes = Number(this.alugavel.valor_mes);
-    
+
     this.alugavel.caracteristicas.forEach(caracteristica => {
       if (caracteristica.tipo_campo.tipo === TIPOS_CAMPOS.BINARIO.nome) caracteristica.valor = translateBoolValue(caracteristica.valor);
     });
     this.resetStatusForm();
+    this.imgUrl = this.alugavel.anunciante_img;
   }
 
   statusChange() {
@@ -62,10 +67,10 @@ export class DetalhesAlugaveisComponent implements OnInit {
 
     this.alugavelService.alterStatus(this.alugavel.id, this.statusForm.value).subscribe(response => {
       this.resetStatusForm()
-      this.snack.open("Salvo com sucesso!", 'OK', {duration: 4000})
+      this.snack.open("Salvo com sucesso!", 'OK', { duration: 4000 })
     }, (error) => {
       console.log(error);
-      this.snack.open("Ocorreu um erro!", 'OK', {duration: 2000})
+      this.snack.open("Ocorreu um erro!", 'OK', { duration: 2000 })
       this.alugavel.disponivel = !this.alugavel.disponivel;
     });
   }
@@ -75,5 +80,11 @@ export class DetalhesAlugaveisComponent implements OnInit {
       status: this.alugavel.status,
       observacao: this.alugavel.observacao
     });
+  }
+
+  public viewUserDetails(id) {
+    console.log(id);
+
+    this.router.navigate([`user/usuarios/${id}`]);
   }
 }
