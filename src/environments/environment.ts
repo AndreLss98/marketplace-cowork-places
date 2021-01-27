c86edb0adbe41d7d1419ff701d0c120ee139e7cd
Victor Monteiro Torres <victoorres@icloud.com>
2021-01-27 15:19:48 -0300

    correção no environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080',
+  apiUrl: 'http://backend.localhost',
   token: '',
   google_api_key: 'AIzaSyBBE4kMR4humYJfPb4MCxqqPF5geXZYAvk',
   paypal_client_id: 'AQF6jES9ny1Kv67WE_LR_ZbU5Mia94YjI1Wen1BQwEogOC2XArKCoO6ihyl1tFJi3_QnpgEyjEGBKIdl',
